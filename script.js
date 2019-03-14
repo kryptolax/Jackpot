cd511e381404a2a04a82c08c6352a03f9160c92a
kryptolax <47044669+kryptolax@users.noreply.github.com>
2019-03-14 12:29:33 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+
+// cycle images in the following order: CHERRY --> LEMON --> ORANGE
+a = document.getElementById('slot1');
+b = document.getElementById('slot2');
+c = document.getElementById('slot3');
+var jp = document.getElementById('you-win');
+
+function nextImageOne(el){
+	if (el.src.match("images/cherry_slot.PNG")){
+		el.src = "images/lemon_slot.PNG";
+		// if the image is a cherry, change it to a lemon!
+	} else if (el.src.match("images/lemon_slot.PNG")){
+		el.src = "images/orange_slot.PNG";
+		// if the image is a lemon, change it to an orange!
+	} else if (el.src.match("images/orange_slot.PNG")){
+		el.src = "images/cherry_slot.PNG";
+		// if the image is an orange, change it to a cherry!
+	} else {
+		// do nothing
+	}
+}
+
+function checkCherries(el) {
+	if (a.src.match('images/cherry_slot.PNG') && b.src.match('images/cherry_slot.PNG') && c.src.match('images/cherry_slot.PNG')){
+		jp.play();
+		console.log("Jackpot!");
+		a.classList.add('opaque')
+		b.classList.add('opaque')
+		c.classList.add('opaque')
+		alert("Jackpot!!!!")
+	} else if (a.src.match('images/lemon_slot.PNG') && b.src.match('images/lemon_slot.PNG') && c.src.match('images/lemon_slot.PNG')){
+		jp.play();
+		console.log("Jackpot!");
+		a.classList.add('opaque')
+		b.classList.add('opaque')
+		c.classList.add('opaque')
+		alert("Jackpot!!!!")
+	} else if (a.src.match('images/orange_slot.PNG') && b.src.match('images/orange_slot.PNG') && c.src.match('images/orange_slot.PNG')){
+		jp.play();
+		console.log("Jackpot!");
+		a.classList.add('opaque')
+		b.classList.add('opaque')
+		c.classList.add('opaque')
+		alert("Jackpot!!!!")
+	} else {
+		//Do Nothing
+	}
+}
+
+function playSound() {
+	// when the JACKPOT! button is clicked, play the slot machine sound
+	document.getElementById('you-win').play();
+}
+
